fe2eac163a4f34681d1f4fea348ecee51a1728af
jrpompio <junior.ruiz@ucr.ac.cr>
2023-11-27 18:08:55 -0600

    hola

--- Proyecto/Derecha-izquierda/testbench.v
+++ Proyecto/Derecha-izquierda/testbench.v
@@ -8,8 +8,10 @@ initial begin
 $dumpvars;
 $monitor("A=%b B=%b N=Z=%b", A0, B0, N0);
 end
-
+for
 tester instancia_datos(.A_valor(A0), .B_valor(B0));
-Inicial_DI instancia_inicial(.A(A0), .B(B0), .N(N0));
+always begin
+Inicial_DI instancia_inicial(.A(A0), .B(B0), .N(N0)); 
+end
 
 endmodule
