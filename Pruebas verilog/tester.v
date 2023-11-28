e71d8aa9197734d811a6f56934d8266db6032822
willie1801 <william.mendezgarcia@ucr.ac.cr>
2023-11-27 19:03:51 -0600

    asdasd

--- Pruebas verilog/tester.v
+++ Pruebas verilog/tester.v
@@ -3,15 +3,13 @@ module tester (
     output reg x, y
 );
 
-  integer contador;
-
   initial begin
-    contador = 0;
-    
-    for (contador = 0; contador < 1; contador = contador + 1)
     begin
-      x = contador;
+      x = 1;
       y = 1;
+      #10
+      x=1;
+      y=0;
     end
   end
 
