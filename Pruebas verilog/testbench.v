ee08454ed8afe2f473cb5566bb241798a04f3e9e
jrpompio <junior.ruiz@ucr.ac.cr>
2023-11-27 22:06:43 -0600

    correciones

--- Pruebas verilog/testbench.v
+++ Pruebas verilog/testbench.v
@@ -7,6 +7,6 @@ initial begin
 $dumpvars;
 $monitor("x=%b y=%b eq=%b", x0, y0, eq0);
 end
-comparador1bit culopanochoteta(.x(x0), .y(y0), .eq(eq0));
-tester    panochoculoteta(.x(x0), .y(y0), .eq(eq0));
+comparador1bit instancia_comparador(.x(x0), .y(y0), .eq(eq0));
+tester    instancia_datos(.x(x0), .y(y0), .eq(eq0));
 endmodule
