387cf27fc2d8ab34bd06c2d771a3abe8da4a7196
willie1801 <william.mendezgarcia@ucr.ac.cr>
2023-11-29 00:17:33 -0600

    cambio celda tipica

---
+++ Proyecto/Iquierda-derecha/celda_tipica.v
@@ -0,0 +1,19 @@
+module Tipica_ID(
+    m, n, A, B, M, N 
+);
+input wire m, n, A, B; // definición  de entradas de la celda 
+output wire M, N;  // definición de salidas de la celda 
+
+wire s0, s1. s2, s4; // equivalencias para la función 
+
+assign s0 = A | ~B 
+assign s1 = m & s0 
+assign s2 = ~A | B 
+assign s3 = n & s2
+assign M = ~n | s1
+assign N = ~m | s3
+
+/* Se tiene entonces que:
+M = ~n | m & (A | ~B) y 
+N = ~m | n & (~A | B)
+*/
