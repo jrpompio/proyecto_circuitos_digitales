75e3ba17d179064ea5d80541f078b5dded662f59
willie1801 <william.mendezgarcia@ucr.ac.cr>
2023-11-29 00:51:08 -0600

    cambio

--- Proyecto/Iquierda-derecha/celda_final.v
+++ Proyecto/Iquierda-derecha/celda_final.v
@@ -1,3 +1,14 @@
 module Final_ID(
     m, n, A, B, Z;
 );
+input wire m, n, A, B; // definición de entradas de la celda
+output wire Z; // definición de salidas de la celda 
+
+
+wire s0, s1;
+
+assign s0 = ~A | B; 
+assign s1 = n & s2;
+assign Z = ~m | s3;
+
+endmodule
