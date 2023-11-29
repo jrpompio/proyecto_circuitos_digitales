783f99dc957f474e386a1fe388b79a6845abcccb
willie1801 <william.mendezgarcia@ucr.ac.cr>
2023-11-29 00:41:20 -0600

    celda inicial cambio

--- Proyecto/Iquierda-derecha/celda_inicial.v
+++ Proyecto/Iquierda-derecha/celda_inicial.v
@@ -1,4 +1,11 @@
 module Inicial_ID(
     A, B, M, N
 );
-in
+input M, A; // definición de entradas de la celda
+output M, N; // definición de salidas de la celda 
+
+assign M = A | ~B;
+assign N = ~A | B;
+
+
+endmodule 
