3bc55153bab0e7aa7f49da0162e12fd175ad3290
willie1801 <william.mendezgarcia@ucr.ac.cr>
2023-11-29 01:19:43 -0600

    cambio

--- Proyecto/Iquierda-derecha/red_iterativa.v
+++ Proyecto/Iquierda-derecha/red_iterativa.v
@@ -8,14 +8,16 @@ module red_iterativa
 ( 
   input [K-1:0] A, // Entradas en cantidad K
   input [K-1:0] B,
+  output[K-1:0] M,
   output [K-1:0] N, // Salidas en cantidad K
   output Z // Salida final
 );
 
   // Módulo inicial para comparar el bit menos significativo
-  Inicial_DI inicial(
+  Inicial_ID inicial(
     .A(A[0]), // Entrada A de celda inicial
     .B(B[0]), // Entrada B de celda inicial
+    .M(M[0]), // Salida M de celda inicial 
     .N(N[0])  // Salida N de celda inicial
   );
 
@@ -29,11 +31,13 @@ module red_iterativa
   // y así generar K-2 veces la celda tipica 
   begin:
     serie_tipica // Nombre de la serie generada
-    Tipica_DI // Modulo usado
+    Tipica_ID // Modulo usado
     tipico( // nombre de la instancia
+        .m(M[i-1]), // entrada m celda tipica #i; salida M anterior
         .n(N[i-1]),  // entrada n celda tipica #i; Salida N anterior
         .A(A[i]), // Entrada A para celda tipica #i
         .B(B[i]), // Entrada B para celda tipica #i
+        .M(M[i]), // Salida M de la celda típica #i
         .N(N[i]) // Salida N de la celda tipica #i
       );
     end
@@ -42,7 +46,8 @@ module red_iterativa
   
   // Módulo final para comparar el bit más significativo
 //  if (K > 1) begin
-  Final_DI final(
+  Final_ID final(
+    .m(M[K-2]), // entrada m celda final; salida ultima celda tipica
     .n(N[K-2]),  // entrada n celda final; Salida ultima celda tipica
     .A(A[K-1]), // Entrada A para celda final
     .B(B[K-1]), // Entrada B para celda final
--- Proyecto/Iquierda-derecha/testbench.v
+++ Proyecto/Iquierda-derecha/testbench.v
@@ -5,7 +5,7 @@ module testbench;
 
 localparam K = 4; // definición de parametro
                   // Para el tamaño de la palabra
-wire [K-1:0] A0, B0, N0; // Se define la cantidad de pines A, B y N
+wire [K-1:0] A0, B0, M0, N0; // Se define la cantidad de pines A, B M y N
 wire Z; // Se define el pin Z
 
   initial begin // Initial para mostrar datos en pantalla
@@ -26,6 +26,7 @@ wire Z; // Se define el pin Z
   red_iterativa #(K) red( // Uso de la red iterativa
     .A(A0),
     .B(B0),
+    .M(M0),
     .N(N0),
     .Z(Z)
   );
